626731ee2cc69607f811cf943d5fef1f2b944fa4
kmert0@outlook.com <kmert0@outlook.com>
2023-02-20 15:09:55 +0300

    parse JSON

--- app.js
+++ app.js
@@ -6,8 +6,17 @@ const app = express();
 const url =
   "https://api.openweathermap.org/data/2.5/weather?q=London&appid=14b596db390b68c40e74dbbb55bb25d0&units=metric";
 
-https.get(url, function(response){
-    console.log(response);
+https.get(url, function (response) {
+  console.log(response.statusCode);
+
+  response.on("data", function (data) {
+    const weatherData = JSON.parse(data);
+    console.log(weatherData);
+    const temp = weatherData.main.temp;
+    console.log(temp);
+    const description = weatherData.weather[0].description;
+    console.log(description);
+  });
 });
 
 app.get("/", function (req, res) {
