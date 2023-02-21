069bce2572c869ffd0319fa338f2158180a4720c
kmert0@outlook.com <kmert0@outlook.com>
2023-02-21 19:13:29 +0300

    post req to the server

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const https = require("https");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  console.log(req.body.cityName);
+
+  const query = req.body.cityName;
+  const apiKey = "14b596db390b68c40e74dbbb55bb25d0";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=London&appid=14b596db390b68c40e74dbbb55bb25d0&units=metric";
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=metric";
 
   https.get(url, function (response) {
     console.log(response.statusCode);
@@ -20,7 +35,7 @@ app.get("/", function (req, res) {
       const icon = weatherData.weather[0].icon;
       const imageURL = "https://openweathermap.org/img/wn/" + icon + "@2x.png";
       res.write(
-        "<h1>The temperature in London is: " + temp + " degrees Celcius</h1>"
+        "<h1>The temperature in " + query +  " is: " + temp + " degrees Celcius</h1>"
       );
       res.write("<h3>The weather is is currently: " + description + "</h3>");
       res.write("<img src=" + imageURL + ">");
